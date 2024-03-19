bc28043d07dddd6084621cf084e2e2eb8711a270
loisa <105920410+helologic@users.noreply.github.com>
2024-03-19 09:26:37 -0300

    Ok

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const 
+const botões = document.querySelectorAll(".botao");
+
+for (let i = 0; i <  botoes.lenght; i++) {
+botoes{i}.onclick = function () {
+
+  for (let j = 0; j < botões.lenght; j++) {
+    botoes[j].classlist.remove("ativo");
+
+  }
+
+  botoes[i].classList.add("ativo")
+    
