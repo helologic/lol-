50615da7eba026907b4037689c33b96908dc1935
loisa <105920410+helologic@users.noreply.github.com>
2024-04-02 12:36:46 +0000

    okokok

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
+
 const botões = document.querySelectorAll(".botao");
+const botões = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i <  botoes.lenght; i++) {
-botoes{i}.onclick = function () {
+botoes[i].onclick = function () {
 
   for (let j = 0; j < botões.lenght; j++) {
     botoes[j].classlist.remove("ativo");
+    textos[j].classlist.remove("ativo");
 
   }
 
   botoes[i].classList.add("ativo")
-    
+  textos[i].classList.add("ativo")
+}
+}
