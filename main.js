5afc9cdbb6d7c04327fdd80a7a4c11d0bf95bac0
loisa <105920410+helologic@users.noreply.github.com>
2024-05-10 13:10:06 +0000

    atualizaa

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 
 const botões = document.querySelectorAll(".botao");
 const botões = document.querySelectorAll(".aba-conteudo");
-
 for (let i = 0; i <  botoes.lenght; i++) {
 botoes[i].onclick = function () {
-
-  for (let j = 0; j < botões.lenght; j++) {
+ for (let j = 0; j < botões.lenght; j++) {
     botoes[j].classlist.remove("ativo");
     textos[j].classlist.remove("ativo");
 
@@ -15,3 +13,7 @@ botoes[i].onclick = function () {
   textos[i].classList.add("ativo")
 }
 }
+
+const contadores = document.querySelectorAll (".contador");
+const tempoObjetivo1 = new Date ("2024-12-11T23:59:59");
+
