0be8dfe0f7a983e97b392f6376595a934cd05f01
loisa <105920410+helologic@users.noreply.github.com>
2024-05-17 12:20:44 +0000

    i dosooomanythingssgodamn

--- main.js
+++ main.js
@@ -1,52 +1,42 @@
+const botoes = document.querySelectorAll(".botao"); 
+const textos = document.querySelectorAll(".aba-conteudo");
+for(let i = 0; i <botoes.length; i++){
+    botoes[i].onclick = function(){
+        for (let j = 0; j< botoes.length; j++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
 
-const botoes = document.querySelectorAll(".botao");
-const texto = document.querySelectorAll(".aba-conteudo");
-for (let i = 0; i <  botoes.lenght; i++) {
-botoes[i].onclick = function () {
- for (let j = 0; j < botoes.lenght; j++) {
-    botoes[j].classlist.remove("ativo");
-    texto[j].classlist.remove("ativo");
-
-  }
-
-  botoes[i].classList.add("ativo")
-  texto[i].classList.add("ativo")
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
 }
-}
-
-const contadores = document.querySelectorAll (".contador");
-const tempoObjetivo1 = new Date ("2024-12-22T23:59:59");
-const tempoObjetivo2 = new Date ("2024-07-05T23:59:59");
-const tempoObjetivo3 = new Date ("2024-08-01T23:59:59");
-const tempoObjetivo4 = new Date ("2024-12-01T23:59:59");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-12-28T23:59:59");
+const tempoObjetivo2 = new Date("2024-07-05T23:59:59");
+const tempoObjetivo3 = new Date("2024-08-01T23:59:59");
+const tempoObjetivo4 = new Date("2024-12-01T23:59:59");
 
 const tempos = [
   tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
 ]
 
-for (let i=0; i< contadores.lenght; i++){
-  contadores[i].textContent = calculaTempo(tempos[i]);
-
+for (let i=0; i < contadores.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
 }
 
-contaores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
-function calculaTempo(tempoObjetivo){
+function calculaTempo(tempoObjetivo1){
   let tempoAtual = new Date();
-  let tempoFinal = tempoObjetivo1 - tempoAtual
-  let segundos = Math.floor(tempoFinal/1000);
-  let minutos = Math.floor(segundos/60);
+  let tempoFinal = tempoObjetivo1 - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
   let horas = Math.floor(minutos / 60);
   let dias = Math.floor (horas / 24);
-
   segundos %= 60;
   minutos %= 60;
   horas %= 24;
-
-  return dias + "dias"
-   + horas + "horas" 
-   + minutos + "minutos" 
-   + segundos + "segundos";
+  return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
 }
-
 
